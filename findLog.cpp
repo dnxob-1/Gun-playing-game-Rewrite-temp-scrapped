69b6bbd2639e6d55c5fc5faf58b7a6484d7f8ff8
morgan <ff6.6.ffdnxob.pthd.6@gmail.com>
2024-09-26 13:29:20 -0500

    /UP

---
+++ findLog.cpp
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <iostream>
+using namespace std;
+
+void sendToLogging(string line1) {
+
+  string c01 = "Player has played at [0:1]";
+
+  // unset vars, set later
+  //
+
+  /*string c02;
+  string c03;
+  string c1;
+  string c2;
+  string c3;
+  string c4;*/
+
+  if (c01 == line1) {
+    main();
+  }
+}
