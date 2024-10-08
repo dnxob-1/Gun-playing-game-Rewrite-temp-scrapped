03584b2c3737f2140401781bd5721e48f94f2d33
morgan <ff6.6.ffdnxob.pthd.6@gmail.com>
2024-10-08 17:13:00 -0500

    fixing

--- waveFuncs/wave.cpp
+++ waveFuncs/wave.cpp
@@ -2,18 +2,3 @@
 #include "wavePlayer1.h"
 #include "wavePlayer2.h"
 #include <iostream>
-using namespace std;
-
-int wave(int players, char count) {
-
-  string line;
-
-  // make it to so that the player(s) can load their pre-built configure.
-  if (players == 2) {
-    wavePlayer2(line, count);
-  } else {
-    wavePlayer1(line, count);
-  }
-
-  return players;
-}
