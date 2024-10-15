965300eed82f81c9110fb2cfa1988954f8d03592
morgan <ff6.6.ffdnxob.pthd.6@gmail.com>
2024-10-14 20:17:39 -0500

    done messed up

--- waveFuncs/wave.cpp
+++ waveFuncs/wave.cpp
@@ -9,7 +9,7 @@ int wave_init(int waveNum) {
   return waveNum;
 }
 
-void waveRunner(int waveNum, int holder) {
+void waveRunnerForPlayer2(int waveNum) {
 
   player1 Player1;
   player2 Player2;
@@ -25,39 +25,71 @@ void waveRunner(int waveNum, int holder) {
   Zombies.getZombCount(waveNum);
   Ghosts.getGhostCount(waveNum);
 
+  std::cout << "current ghost health: " << ghostHealth << '\n';
+
+  std::cout << "current ghost count: " << Ghosts.ghostCount << '\n';
+
   for (int i = 0; i < Brutus.bruteCount; i++) {
     Brutus.killBrute();
-    if (holder == 2) {
-      Player2.damage(Brutus.bruteCount, bruteHealth);
-      Brutus.damagePlayerTwo(Player2);
-    } else {
-      Player1.damage(Brutus.bruteCount, bruteHealth);
-      Brutus.damagePlayerOne(Player1);
-    }
+    std::cout << "current bruteCount: " << Brutus.bruteCount << '\n';
+    Player2.damage(Brutus.bruteCount, bruteHealth);
+    std::cout << "current brute health: " << bruteHealth << '\n';
     Brutus.damagePlayerTwo(Player2);
     for (int j = 0; i < Zombies.zombCount; j++) {
       Zombies.killZomb();
-      if (holder == 2) {
-        Player2.damage(Zombies.zombCount, zombHealth);
-        Zombies.damagePlayerTwo(Player2);
-      } else {
-        Zombies.damagePlayerOne(Player1);
-        Player1.damage(Zombies.zombCount, zombHealth);
-      }
+      Player2.damage(Zombies.zombCount, zombHealth);
       Zombies.damagePlayerTwo(Player2);
-      for (int p = 0; p < Ghosts.ghostCount; p++) {
-        Ghosts.killGhosts();
-        if (holder == 2) {
-          Ghosts.damagePlayerTwo(Player2);
-          Player2.damage(Ghosts.ghostCount, ghostHealth);
-        } else {
-          Player1.damage(Ghosts.ghostCount, ghostHealth);
-          Ghosts.damagePlayerOne(Player1);
-        }
-        Ghosts.damagePlayerTwo(Player2);
-      }
+    }
+    Zombies.damagePlayerTwo(Player2);
+    for (int p = 0; p < Ghosts.ghostCount; p++) {
+      Ghosts.killGhosts();
+      Ghosts.damagePlayerTwo(Player2);
+      Player2.damage(Ghosts.ghostCount, ghostHealth);
+    }
+    Ghosts.damagePlayerTwo(Player2);
+  }
+  std::cout << ghostHealth << '\n';
+}
+
+void waveRunnerForPlayer1(int waveNum) {
+
+  player1 Player1;
+  player2 Player2;
+  brutus Brutus;
+  zombies Zombies;
+  ghosts Ghosts;
+
+  int ghostHealth = Ghosts.accessToHealth();
+  int zombHealth = Zombies.accessToHealth();
+  int bruteHealth = Brutus.accessToHealth();
+
+  Brutus.getBruteCount(waveNum);
+  Zombies.getZombCount(waveNum);
+  Ghosts.getGhostCount(waveNum);
+
+  std::cout << "current ghost health: " << ghostHealth << '\n';
+
+  std::cout << "current ghost count: " << Ghosts.ghostCount << '\n';
+
+  for (int i = 0; i < Brutus.bruteCount; i++) {
+    Brutus.killBrute();
+    std::cout << "current bruteCount: " << Brutus.bruteCount << '\n';
+    Player1.damage(Brutus.bruteCount, bruteHealth);
+    std::cout << "current brute health: " << bruteHealth << '\n';
+    Brutus.damagePlayerOne(Player1);
+    for (int j = 0; i < Zombies.zombCount; j++) {
+      Zombies.killZomb();
+      Zombies.damagePlayerOne(Player1);
+      Player1.damage(Zombies.zombCount, zombHealth);
+    }
+    Zombies.damagePlayerTwo(Player2);
+    for (int p = 0; p < Ghosts.ghostCount; p++) {
+      Ghosts.killGhosts();
+      Player1.damage(Ghosts.ghostCount, ghostHealth);
+      Ghosts.damagePlayerOne(Player1);
     }
   }
+  std::cout << ghostHealth << '\n';
 }
 
 void wave(int waveNum) {
@@ -73,14 +105,13 @@ void wave(int waveNum) {
   if (Holder == 2) {
     std::cout << "Player " << Holder << " has been chosen, who currently has "
               << Player2.accessToHealth() << " health Points" << '\n';
-    waveRunner(waveNum, Holder);
-
+    waveRunnerForPlayer2(waveNum);
   } else if (Holder == 1) {
 
     std::cout << "Player " << Holder << " has been chosen, who currently has "
               << Player1.accessToHealth() << " health Points" << '\n';
 
-    waveRunner(waveNum, Holder);
+    waveRunnerForPlayer1(waveNum);
   } else {
     // make it so that the player count matters here
     std::cout << "An error has occured!" << '\n';
